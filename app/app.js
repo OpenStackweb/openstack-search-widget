5e1120e44aefe61de8b4c1306f16194c4572841a
Santi <santipalenque@gmail.com>
2018-06-26 18:49:52 -0300

    [spalenque] - WIP

--- app/app.js
+++ app/app.js
@@ -1,43 +1,52 @@
 define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget.css'], function ($, Ractive, mainTemplate, css) {
 
-  'use strict';
-  
-  var app = {
-    init: function () {
+    'use strict';
 
-    var $style = $("<style></style>", {type: "text/css"});
-    $style.text(css);
-    $("head")
-        .append($style)
-        .append('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" >');
+    var app = {
+        init: function () {
 
-      // render our main view
-      this.ractive = new Ractive({
-        el: 'openstack-search-bar',
-        template: mainTemplate,
-        data: {
-          cnt: 0,
-          ts: 'never',
+            var $style = $("<style></style>", {type: "text/css"});
+            $style.text(css);
+
+            $("head")
+                .append($style)
+                .append('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" >');
+
+            // render our main view
+            this.ractive = new Ractive({
+                el: 'openstack-search-bar',
+                template: mainTemplate,
+                data: {
+                    term: ''
+                }
+            });
+
+            this.ractive.on({
+                clear: function(ev) {
+                    ev.original.preventDefault();
+                    this.set('term', '');
+                },
+                search: function(ev) {
+                    ev.original.preventDefault();
+                    if(ev.original.keyCode == 13) {
+                        $('.search-results').show();
+                    } else {
+                        window.setTimeout(doSearch, 500, this.get('term'));
+                    }
+                },
+                closePopup: function(ev) {
+                    $('.search-results').hide();
+                }
+            });
+
+
         }
-      });
-      this.ractive.on({
-        mwClick: function(ev) {
-          ev.original.preventDefault()
-          this.set('cnt', this.get('cnt') + 1);
-          var that = this;
-          $.ajax({
-            url: "http://date.jsontest.com/",
-            dataType: "jsonp"
-          }).then(function(resp) {
-            that.set("ts", resp.time);
-          }, function(resp) {
-            that.set("ts", "Something bad happened");
-          });
-        }
-      });
+    };
+
+    function doSearch(term) {
+        alert(term);
     }
-  };
 
-  return app;
+    return app;
 
 });
