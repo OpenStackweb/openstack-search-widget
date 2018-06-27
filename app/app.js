e8a442c63c99abd01eefe7f9828ab4712be6cc3a
Santi <santipalenque@gmail.com>
2018-06-27 18:24:38 -0300

    [spalenque] - pagination WIP

--- app/app.js
+++ app/app.js
@@ -17,7 +17,22 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget.css'],
                 el: 'openstack-search-bar',
                 template: mainTemplate,
                 data: {
-                    term: ''
+                    term: '',
+                    page: 1,
+                    perPage: 10,
+                    totalPages: 15,
+                    pagesToShow: [1,2,3,4,5],
+                    fromResult: 1,
+                    toResult: 10,
+                    total: 300,
+                    results: [
+                        {
+                            link: 'http://google.com',
+                            title: 'heat output-show my-<em>kube</em>-cluster <em>kube_minions_external</em>',
+                            detail: 'First time here? Check out the FAQ! Chinese English ▼ Hi there! Please sign in tags users badges help...'
+                        },
+
+                    ]
                 }
             });
 
@@ -27,15 +42,46 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget.css'],
                     this.set('term', '');
                 },
                 search: function(ev) {
+                    let term = this.get('term');
                     ev.original.preventDefault();
                     if(ev.original.keyCode == 13) {
                         $('.search-results').show();
                     } else {
-                        window.setTimeout(doSearch, 500, this.get('term'));
+                        window.setTimeout(doSearch, 500, term);
                     }
                 },
                 closePopup: function(ev) {
                     $('.search-results').hide();
+                },
+                changePage: function(ev, newPage) {
+                    let {pagesToShow, totalPages, perPage, total} = this.get();
+                    let lastPage = pagesToShow[pagesToShow.length - 1];
+
+                    ev.original.preventDefault();
+                    this.set('page', newPage);
+
+                    // change results
+                    let newResultLimit = (newPage * perPage) + perPage;
+                    newResultLimit = (newResultLimit > total) ? total : newResultLimit;
+                    this.set('fromResult', (newPage * perPage));
+                    this.set('toResult', newResultLimit);
+
+                    // change pagination
+                    if (newPage > lastPage - 2) {
+                        let newPagesToShow = [];
+                        let pageFrom = newPage - 2;
+                        let pageTo = newPage + 2;
+
+                        if (pageTo > totalPages) {
+                          pageTo = totalPages;
+                          pageFrom = pageTo - 5;
+                        }
+
+                        for( var i = pageFrom; i < pageTo; i++) {
+                            newPagesToShow.push(i);
+                        }
+                        this.set('pagesToShow', newPagesToShow);
+                    }
                 }
             });
 
